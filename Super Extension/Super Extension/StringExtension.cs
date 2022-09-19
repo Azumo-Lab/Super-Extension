bc815ba52a555ed5409f5fe2ea2295d4af8941b1
牛奶 <burpee_soggier0x@icloud.com>
2022-09-19 17:15:42 +0800

    初始化提交

---
+++ Super Extension/Super Extension/StringExtension.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace SuperExtension
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 判断是否是Null，Empty，空格
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmptyOrWhiteSpace(this string str) =>
+            str == null || str.Length == 0 || str.Trim().Length == 0;
+
+        /// <summary>
+        /// 转换
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static T Conver<T>(this string str)
+        {
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            if (!str.IsNullOrEmptyOrWhiteSpace())
+                try
+                {
+                    return (T)converter.ConvertFromString(str);
+                }
+                catch (Exception)
+                {}
+            return default;
+        }
+
+        /// <summary>
+        /// 删除字符串中的空格
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string DeleteWhitespace(this string str)
+        {
+            if (str.IsNullOrEmptyOrWhiteSpace())
+                return string.Empty;
+
+            return string.Join(string.Empty, str.Where(item => !char.IsWhiteSpace(item)));
+        }
+
+        /// <summary>
+        /// 比较两个字符串，不区分大小写
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        /// <returns></returns>
+        public static bool EqualsIgnoreCase(this string str1, string str2)
+        {
+            return str1?.ToLower() == str2?.ToLower();
+        }
+
+        /// <summary>
+        /// 删除字符串前面和后面的空格
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string TrimStartAndEnd(this string str)
+        {
+            if (str.IsNullOrEmptyOrWhiteSpace())
+                return string.Empty;
+            return str.TrimStart().TrimEnd();
+        }
+
+        /// <summary>
+        /// 将文本内容存入文件
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="Path"></param>
+        /// <param name="encoding"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void SaveToFile(this string str, string Path, Encoding encoding)
+        {
+            if(Path.IsNullOrEmptyOrWhiteSpace())
+                throw new ArgumentNullException(nameof(Path));
+
+            string DirPath = System.IO.Path.GetDirectoryName(Path);
+            if (!DirPath.IsNullOrEmptyOrWhiteSpace())
+                Directory.CreateDirectory(DirPath);
+            
+            using (FileStream fs = new FileStream(Path, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, encoding))
+                {
+                    sw.Write(str ?? string.Empty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 将文本内容存入文件
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="Path"></param>
+        public static void SaveToFile(this string str, string Path)
+        {
+            str.SaveToFile(Path, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// 文字编码变换
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="InputEncoding"></param>
+        /// <param name="OutPutEncoing"></param>
+        /// <returns></returns>
+        public static string EncodingChange(this string str, Encoding InputEncoding, Encoding OutPutEncoing)
+        {
+            return OutPutEncoing.GetString(InputEncoding.GetBytes(str));
+        }
+    }
+}
---
+++ Super Extension/Super ExtensionTests/StringExtensionTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperExtension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperExtension.Tests
+{
+    [TestClass()]
+    public class StringExtensionTests
+    {
+        [TestMethod()]
+        public void IsNullOrEmptyOrWhiteSpaceTest()
+        {
+            string? testVal = null;
+            Assert.IsTrue(testVal.IsNullOrEmptyOrWhiteSpace());
+            Assert.IsTrue("".IsNullOrEmptyOrWhiteSpace());
+            Assert.IsTrue("      ".IsNullOrEmptyOrWhiteSpace());
+            Assert.IsFalse("          aa".IsNullOrEmptyOrWhiteSpace());
+            Assert.IsFalse("    11     ".IsNullOrEmptyOrWhiteSpace());
+            Assert.IsFalse("123".IsNullOrEmptyOrWhiteSpace());
+        }
+
+        [TestMethod()]
+        public void ConverTest()
+        {
+            string? testVal = null;
+            Assert.IsTrue(default == testVal.Conver<int>());
+            Assert.IsTrue(default == "".Conver<int>());
+            Assert.IsTrue(default == "   ".Conver<int>());
+            Assert.IsTrue(default == "   ff".Conver<int>());
+            Assert.IsTrue(1 == "1".Conver<int>());
+            Assert.IsTrue(default == "".Conver<object>());
+        }
+
+        [TestMethod()]
+        public void DeleteWhitespaceTest()
+        {
+            string? testVal = null;
+            Assert.IsTrue(string.Empty == testVal.DeleteWhitespace());
+            Assert.IsTrue(string.Empty == "".DeleteWhitespace());
+            Assert.IsTrue(string.Empty == "     ".DeleteWhitespace());
+            Assert.IsTrue("E" == "    E    ".DeleteWhitespace());
+            Assert.IsTrue("123" == "1   2    3    ".DeleteWhitespace());
+        }
+
+        [TestMethod()]
+        public void EqualsIgnoreCaseTest()
+        {
+            string? testVal = null;
+            Assert.IsFalse(testVal.EqualsIgnoreCase("Hello"));
+            Assert.IsFalse(testVal.EqualsIgnoreCase(" 55"));
+            Assert.IsFalse(testVal.EqualsIgnoreCase(""));
+            Assert.IsFalse("Hell".EqualsIgnoreCase(null));
+            Assert.IsTrue("HELLO".EqualsIgnoreCase("HELLO"));
+            Assert.IsTrue("HellO".EqualsIgnoreCase("hello"));
+        }
+
+        [TestMethod()]
+        public void TrimStartAndEndTest()
+        {
+            string? testVal = null;
+            Assert.IsTrue(testVal.TrimStartAndEnd() == string.Empty);
+            Assert.IsTrue("".TrimStartAndEnd() == string.Empty);
+            Assert.IsTrue("    ".TrimStartAndEnd() == string.Empty);
+            Assert.IsTrue("     1".TrimStartAndEnd() == "1");
+            Assert.IsTrue("2".TrimStartAndEnd() == "2");
+            Assert.IsTrue("1 2 3 4".TrimStartAndEnd() == "1 2 3 4");
+            Assert.IsTrue("1 2   .".TrimStartAndEnd() == "1 2   .");
+            Assert.IsTrue("  1 2   ".TrimStartAndEnd() == "1 2");
+        }
+
+        [TestMethod()]
+        public void SaveToFileTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                string? str = null;
+                str.SaveToFile(null);
+            });
+            "Hello".SaveToFile("Test.txt");
+            Assert.IsTrue(File.Exists("Test.txt"));
+            Assert.IsTrue("Hello" == File.ReadAllText("Test.txt"));
+            File.Delete("Test.txt");
+        }
+
+        [TestMethod()]
+        public void SaveToFileTest1()
+        {
+            string changeStr = "こんにちは".EncodingChange(Encoding.UTF8, Encoding.Unicode);
+
+            changeStr.SaveToFile("Test00.txt", Encoding.Unicode);
+
+            Assert.IsTrue(changeStr == File.ReadAllText("Test00.txt", Encoding.Unicode));
+            File.Delete("Test00.txt");
+        }
+    }
+}
