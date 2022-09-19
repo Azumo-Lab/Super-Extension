f3f49b1380485884e4443cf99d9d9465b105283d
牛奶 <burpee_soggier0x@icloud.com>
2022-09-19 17:35:06 +0800

    更新：添加IEnumerable扩展

---
+++ Super Extension/Super Extension/IEnumerableExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SuperExtension
+{
+    public static class IEnumerableExtension
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="If"></param>
+        /// <param name="Then"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, bool If, Func<TSource, bool> Then)
+        {
+            return If ? source.Where(Then) : source;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="path"></param>
+        /// <param name="encoding"></param>
+        public static void SaveToFile(this IEnumerable<string> source, string path, Encoding encoding)
+        {
+            string DirPath = Path.GetDirectoryName(path);
+            if (!DirPath.IsNullOrEmptyOrWhiteSpace())
+                Directory.CreateDirectory(DirPath);
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, encoding))
+                {
+                    source.ForEach(x => sw.WriteLine(x));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="souce"></param>
+        /// <param name="action"></param>
+        public static void ForEach(this IEnumerable<string> souce, Action<string> action)
+        {
+            foreach (string item in souce)
+                action(item);
+        }
+    }
+}
