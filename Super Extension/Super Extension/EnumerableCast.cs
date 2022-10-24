7ebadd6cfd64e13ccd0d90b4177edab7ed5929a0
牛奶 <burpee_soggier0x@icloud.com>
2022-10-24 13:22:12 +0800

    更新：添加一些扩展方法

---
+++ Super Extension/Super Extension/EnumerableCast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SuperExtension
+{
+    public static class EnumerableCast
+    {
+        /// <summary>
+        /// 转换
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> CastToIEnumerable<TSource>(this IEnumerable source)
+        {
+            return from object item in source
+                   select (TSource)item;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static List<TSource> CastToList<TSource>(this IEnumerable source)
+        {
+            return CastToIEnumerable<TSource>(source).ToList();
+        }
+    }
+}
---
+++ Super Extension/Super ExtensionTests/EnumerableCastTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperExtension;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperExtension.Tests
+{
+    [TestClass()]
+    public class EnumerableCastTests
+    {
+        [TestMethod()]
+        public void CastToIEnumerableTest()
+        {
+            var aa = new ArrayList();
+            aa.Add("Test");
+            aa.Add("Test BB");
+
+            List<string> strs = aa.CastToIEnumerable<string>().ToList();
+
+            Assert.AreEqual(2, strs.Count);
+            Assert.AreEqual("Test", strs[0]);
+            Assert.AreEqual("Test BB", strs[1]);
+        }
+    }
+}
